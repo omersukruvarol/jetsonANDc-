359416403679207cd4a5a8a70445041b3af25b2f
omersukruvarol <sukru.varol@bilgiedu.net>
2023-11-20 17:53:51 +0300

    Update jetson.py

--- jetson.py
+++ jetson.py
@@ -1,26 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
 from ultralytics import YOLO
+from PIL import Image
+import io
 
 model = YOLO('best (4).pt')
 app = FastAPI()
 
 @app.post("/predict/")
-
-
-
-
 async def predict(image: bytes):
-    # Convert the image bytes to a PIL Image
-    image = Image.open(io.BytesIO(image))
+    try:
+        # Convert the image bytes to a PIL Image
+        image = Image.open(io.BytesIO(image))
 
-    # Make the prediction using YOLOv5
-    predictions = model(image)
+        # Make the prediction using YOLOv5
+        predictions = model(image)
 
-    # Convert the predictions to a byte array
-    prediction_bytes = predictions.toImage().tobytes()
+        # Convert the predictions to a byte array
+        prediction_bytes = predictions.render().tobytes()
 
-    return prediction_bytes
-    
+        return prediction_bytes
+    except Exception as e:
+        return {"error": str(e)}  # Handle any exceptions and return an error message
+
 if __name__ == "__main__":
     import uvicorn
 
